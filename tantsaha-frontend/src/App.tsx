e70c613fb271ebcdf0ed195ab41137af7f5e6a38
fenohasina <fenohasinaherimamy@gmail.com>
2025-12-21 09:07:29 +0300

    feat: tailwindcss ok

---
+++ tantsaha-frontend/src/App.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+ 
+
+function App() {
+  return (
+    <div className="min-h-screen p-8 bg-green-100">
+      <h1 className="mb-4 text-4xl font-bold text-green-800 text-blue-500" >
+        ✅ Tantsaha Connect
+      </h1>
+      <p className="text-lg">
+        Si ce texte est vert et avec un fond vert clair, Tailwind fonctionne !
+      </p>
+    </div>
+  );
+}
+
+export default App;
