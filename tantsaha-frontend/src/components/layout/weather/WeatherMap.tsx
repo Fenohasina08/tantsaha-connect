9b9f7cd0ea9265ca79fbe87373aa9107c560ca10
fenohasina <fenohasinaherimamy@gmail.com>
2026-01-11 19:09:54 +0300

    feat: initiation of color

--- tantsaha-frontend/src/components/layout/weather/WeatherMap.tsx
+++ tantsaha-frontend/src/components/layout/weather/WeatherMap.tsx
@@ -1,5 +1,6 @@
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 import L from 'leaflet';
+import { number } from 'framer-motion';
 
 // 1. Les données des villes (on peut les mettre ici pour l'instant)
  const MADAGASCAR_CITIES = [
@@ -23,18 +24,47 @@ import L from 'leaflet';
   }
 ];
 
+ const createWeatherIcon = (temp: number) => {
+  // Définition de la couleur selon la température 🌡️
+  const color = temp >= 30 ? '#ef4444' : temp >= 20 ? '#fb923c' : '#3b82f6';
+
+  return L.divIcon({
+    className: "custom-icon",
+    html: `<div style="background-color: ${color};" 
+               class="w-6 h-6 rounded-full border-2 border-white shadow-lg flex items-center justify-center text-[10px] text-white font-bold">
+            ${temp}
+          </div>`,
+    iconSize: [24, 24],
+    iconAnchor: [12, 12]
+  });
+};
+
 export default function WeatherMap() {
   return (
     <div className="h-[400px] w-full rounded-xl overflow-hidden shadow-lg">
-      <MapContainer center={[-18.7669, 46.8691]} zoom={5} style={{ height: '100%', width: '100%' }}>
+       <MapContainer center={[-18.7669, 46.8691]} zoom={5} style={{ height: '100%', width: '100%' }}>
         <TileLayer
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-          attribution='&copy; OpenStreetMap contributors'
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            attribution='&copy; OpenStreetMap contributors'
         />
-        
-        {/* C'est ici que nous allons afficher les points sur la carte */}
-        
-      </MapContainer>
+  
+  {/* On insère la boucle ici */}
+  {MADAGASCAR_CITIES.map((city) => (
+  <Marker 
+    key={city.name} 
+    position={city.coordinates as [number, number]}
+    icon={createWeatherIcon(city.temp)} // On appelle notre fonction ici ! 🎨
+  >
+    <Popup>
+      <div className="text-center">
+        <p className="font-bold">{city.name}</p>
+        <p className="text-sm">{city.region}</p>
+        <p className="font-semibold text-blue-600">{city.temp}°C</p>
+      </div>
+    </Popup>
+  </Marker>
+  ))}
+</MapContainer>
     </div>
   );
 }
