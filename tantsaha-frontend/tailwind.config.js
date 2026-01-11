d39526cdfefbcff039bae58980a44979a4256fb9
fenohasina <fenohasinaherimamy@gmail.com>
2026-01-11 14:48:02 +0300

    feat: animation to tailwind

--- tantsaha-frontend/tailwind.config.js
+++ tantsaha-frontend/tailwind.config.js
@@ -4,7 +4,24 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      // 1. On définit les mouvements 🎬
+      keyframes: {
+        fall: {
+          '0%': { transform: 'translateY(-100%)' },
+          '100%': { transform: 'translateY(100vh)' },
+        },
+        drift: {
+          '0%': { transform: 'translateX(-20%)' },
+          '100%': { transform: 'translateX(120%)' },
+        }
+      },
+      // 2. On crée les noms de classes utilisables 🚀
+      animation: {
+        rain: 'fall 0.8s linear infinite',
+        cloud: 'drift 30s linear infinite',
+      }
+    },
   },
   plugins: [],
 }
